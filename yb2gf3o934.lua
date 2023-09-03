57f1e50c755e1f617c2e4d3da5fcea020e9ee1e8
Burapha Jaigla <73657995+burapha44@users.noreply.github.com>
2023-09-03 21:44:07 +0700

    Update yb2gf3o934.lua

--- yb2gf3o934.lua
+++ yb2gf3o934.lua
@@ -1,1 +1,10 @@
-
+local gamelist = {
+    [994732206]  = "",
+}
+local checkgame = gamelist[game.gameId]
+if gamelist[game.gameId] then 
+print (checkgame)
+    loadstring(game:HttpGet(checkgame))()
+else 
+game.Players.LocalPlayer:Kick("Not Game Support")
+end
