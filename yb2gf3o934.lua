cd834331486ea3237978e03bf72337da639b3b79
Burapha Jaigla <73657995+burapha44@users.noreply.github.com>
2023-09-08 23:31:33 +0700

    Update yb2gf3o934.lua

--- yb2gf3o934.lua
+++ yb2gf3o934.lua
@@ -1,5 +1,5 @@
 local gamelist = {
-    [994732206]  = "",
+    [994732206]  = "https://raw.githubusercontent.com/easykub/loader/master/bf.lua",
 }
 local checkgame = gamelist[game.gameId]
 if gamelist[game.gameId] then 
